ffc6023ac171ec44777d550cbe48952c24cc8876
ruy2104kkgg09-dotcom <ruy2104kkgg09@gmail.com>
2026-06-20 09:45:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kuruma-app-20260620015224";
+const CACHE_NAME = "kuruma-app-20260620092558";
 const ASSETS = ["./","index.html","manifest.json",
   "assets/icons/icon-192.png","assets/icons/icon-512.png",
   "assets/icons/icon-maskable-192.png","assets/icons/icon-maskable-512.png",
